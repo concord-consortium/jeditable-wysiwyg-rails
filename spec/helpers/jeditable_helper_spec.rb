6acc858ef6fdc33af26d8eb80b132a6f8a21e0a7
Parker Morse <flashesofpanic@gmail.com>
2012-11-20 10:08:41 -0500

    Lots of pending tests

--- spec/helpers/jeditable_helper_spec.rb
+++ spec/helpers/jeditable_helper_spec.rb
@@ -6,23 +6,35 @@ describe JeditableHelper do
       gadget = Gadget.new(:name => 'First')
       helper.editable_field(gadget, :name).should =~ /First/
     end
+
+    it 'uses put by default'
+
+    it 'uses post when supplied that parameter'
+
+    it 'calculates a name parameter when none is supplied'
+
+    it 'uses a supplied name parameter'
+
+    it 'calculates a target URL from the object'
+
+    it 'uses a supplied URL parameter'
+
+    context 'when editing is triggered by a control' do
+      it 'binds edit action to edit-click'
+
+      it 'shows a trigger with the appropriate text'
+    end
+
+    context 'when open-in-edit is requested' do
+      it 'opens in edit mode if value is blank'
+
+      it 'does not open in edit mode if value exists'
+    end
   end
 
-  describe 'editable_with_trigger' do
-    before do
-      @gadget = Gadget.new(:name => 'First')
-    end
+  describe 'editable_field_if' do
+    it 'shows an editable field when condition is true'
 
-    it "displays the value of the object" do
-      helper.editable_with_trigger(@gadget, :name).should =~ /First/
-    end
-
-    it "displays an edit-triggering span" do
-      helper.editable_with_trigger(@gadget, :name).should =~ /span class="edit_trigger"/
-    end
-
-    it "uses the edit_string option as the text of the edit-triggering span" do
-      helper.editable_with_trigger(@gadget, :name, { :edit_string => 'Yo, edit this!'}).should =~ /Yo, edit this!/
-    end
+    it 'shows the field value with no function bound to click when condition is false'
   end
 end
