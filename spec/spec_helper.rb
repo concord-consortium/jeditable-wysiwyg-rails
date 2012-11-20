d48d222954aaa39bca65c9e090dab496621cdf95
Parker Morse <flashesofpanic@gmail.com>
2012-11-20 10:09:16 -0500

    WIP trying to get specs to work (for #6)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,16 @@
 # Configure Rails Envinronment - figured this out via http://stackoverflow.com/questions/5374504/rspec-gem-development-stubbing-rails-templates/6272019#6272019
 ENV["RAILS_ENV"] = "test"
+ENV["RAILS_ROOT"] = File.expand_path("../dummy",  __FILE__)
+
 require File.join(File.dirname(__FILE__), 'dummy', 'config', 'environment.rb')
+require File.join(File.dirname(__FILE__), 'dummy', 'config', 'routes.rb')
+require 'rails/test_help'
+
+require 'rspec/rails'
 
 require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
-
-require 'rails/test_help'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
