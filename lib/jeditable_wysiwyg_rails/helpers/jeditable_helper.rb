e075b563b5b8280a1f385b8c45c86a597d60ab31
Parker Morse <flashesofpanic@gmail.com>
2012-12-03 13:09:56 -0500

    Bugfixes, making the helper more testable.

--- lib/jeditable_wysiwyg_rails/helpers/jeditable_helper.rb
+++ lib/jeditable_wysiwyg_rails/helpers/jeditable_helper.rb
@@ -32,13 +32,15 @@ module JeditableHelper
   #   for a trigger event.
   def editable_field(object, property, options={})
     name = "#{object.class.to_s.underscore}[#{property}]"
+    property_name = "#{object.class.to_s.underscore}_#{property}"
+    trigger_id = "#{property_name}_trigger"
     value = object.send property
     update_url = options.delete(:update_url) || url_for(object)
     trigger_event = 'click'
     trigger_reset = ''
     if options[:use_trigger]
       trigger_event = 'edit-click'
-      trigger_reset = "{onreset: window.showTrigger_#{object.class.to_s.underscore}_#{property}, onsubmit: window.showTrigger_#{object.class.to_s.underscore}_#{property}}"
+      trigger_reset = "{onreset: window.showTrigger_#{property_name}, onsubmit: window.showTrigger_#{property_name} }"
     end
     open_in_edit = (options[:open_if_empty] && value.blank?) ? ".trigger('#{trigger_event}')" : ''
     visible_trigger = !(options[:open_if_empty] && value.blank?)
@@ -61,18 +63,18 @@ module JeditableHelper
             $(".editable[data-id='#{object.id}'][data-name='#{name}']").editable("#{update_url}", args)#{open_in_edit};
           });
         })( jQuery );
-        function showTrigger_#{object.class.to_s.underscore}_#{property}(settings, original) {
+        function showTrigger_#{property_name}(settings, original) {
           /* The edit trigger, if it exists, is hidden when edit mode begins; this function restores it if needed. */
-          $(".edit_trigger[id='#{name}_trigger']").toggle();
+          $(".edit_trigger[id='#{trigger_id}']").toggle();
         }
       </script>
-      #{editable_trigger(name, options[:edit_string], object.id, visible_trigger) if options[:use_trigger]}
+      #{editable_trigger(name, property_name, options[:edit_string], object.id, visible_trigger) if options[:use_trigger]}
     }.html_safe
   end
 
   # Creates a trigger to open edit mode on an editable span.
-  def editable_trigger(name, edit_string, object_id, visible=true)
-    trigger_name = "#{name}_trigger"
+  def editable_trigger(name, property, edit_string, object_id, visible=true)
+    trigger_name = "#{property}_trigger"
     visibility = visible ? '' : 'style="display: none"'
     %{
       <span class="edit_trigger" id="#{trigger_name}" #{visibility}>#{edit_string}</span>
