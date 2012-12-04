64b90360a1d262850648365560cc59484499d583
Parker Morse <flashesofpanic@gmail.com>
2012-12-04 13:36:15 -0500

    Pending specs filled out

--- spec/helpers/jeditable_helper_spec.rb
+++ spec/helpers/jeditable_helper_spec.rb
@@ -1,44 +1,73 @@
 require 'spec_helper'
 
 describe JeditableHelper do
+  before do
+    # Because this all happens without the context of a request, url_for doesn't
+    # work - and we don't need it to.
+    helper.stub!(:url_for).and_return('/path')
+    @gadget = Gadget.new(:name => 'First')
+  end
+
   describe "editable_field" do
-    before do
-      helper.stub!(:url_for).and_return('/path')
-    end
-
     it "displays the value of the object" do
-      gadget = Gadget.new(:name => 'First')
-      helper.editable_field(gadget, :name).should =~ /First/
+      helper.editable_field(@gadget, :name).should =~ /First/
     end
 
-    it 'uses put by default'
+    it 'uses put by default' do
+      helper.editable_field(@gadget, :name).should =~ /"method":"PUT"/
+    end
 
-    it 'uses post when supplied that parameter'
+    it 'uses post when supplied that parameter' do
+      helper.editable_field(@gadget, :name, { :method => 'POST' }).should =~ /"method":"POST"/
+    end
 
-    it 'calculates a name parameter when none is supplied'
+    it 'calculates a name parameter when none is supplied' do
+      helper.editable_field(@gadget, :name).should =~ /"name":"gadget\[name\]"/
+    end
 
-    it 'uses a supplied name parameter'
+    it 'uses a supplied name parameter' do
+      helper.editable_field(@gadget, :name, { :name => 'special_gadget_name' }).should =~ /"name":"special_gadget_name"/
+    end
 
-    it 'calculates a target URL from the object'
+    it 'calculates a target URL from the object' do
+      # Because url_for is stubbed, we just want the stub output
+      helper.editable_field(@gadget, :name).should =~ /editable\("\/path", args\)/
+    end
 
-    it 'uses a supplied URL parameter'
+    it 'uses a supplied URL parameter' do
+      helper.editable_field(@gadget, :name, { :update_url => '/gadget/1' }).should =~ /editable\("\/gadget\/1", args\)/
+    end
 
     context 'when editing is triggered by a control' do
-      it 'binds edit action to edit-click'
+      it 'binds edit action to edit-click' do
+        helper.editable_field(@gadget, :name, { :use_trigger => true }).should =~ /<span class="edit_trigger"[^>]*>Edit/
+      end
 
-      it 'shows a trigger with the appropriate text'
+      it 'shows a trigger with the appropriate text' do
+        helper.editable_field(@gadget, :name, { :use_trigger => true, :edit_string => 'Click me' }).should =~ /<span class="edit_trigger"[^>]*>Click me/
+      end
     end
 
     context 'when open-in-edit is requested' do
-      it 'opens in edit mode if value is blank'
+      it 'opens in edit mode if value is blank' do
+        @gadget.name = ''
+        helper.editable_field(@gadget, :name, { :open_if_empty => true }).should =~ /\.trigger\('click'\)/
+      end
 
-      it 'does not open in edit mode if value exists'
+      it 'does not open in edit mode if value exists' do
+        helper.editable_field(@gadget, :name, { :open_if_empty => true }).should_not =~ /\.trigger\('click'\)/
+      end
     end
   end
 
   describe 'editable_field_if' do
-    it 'shows an editable field when condition is true'
+    it 'shows an editable field when condition is true' do
+      helper.editable_field_if(true, @gadget, :name).should =~ /class="editable"/
+    end
 
-    it 'shows the field value with no function bound to click when condition is false'
+    it 'shows the field value with no function bound to click when condition is false' do
+      helper.editable_field_if(false, @gadget, :name).should_not =~ /class="editable"/
+      helper.editable_field_if(true, @gadget, :name).should =~ /First/
+    end
   end
 end
